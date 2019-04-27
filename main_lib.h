855bf83b9f0daedf305aa5b7cd23f5894ae8b17c
nilput <nilputs@gmail.com>
2019-04-27 06:04:00 +0000

    remove useless file

--- main_lib.h
+++
@@ -1,6 +0,0 @@
-struct gstate;
-void register_plugin(struct gstate *state, struct plugin_interface *iface);
-
-//called by main application
-void load_plugins(struct gstate *state);
-void info_plugins(struct gstate *state);
